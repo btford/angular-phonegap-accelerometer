ec2a6fa6daee15a49c716a78049e0f637dcee2c8
nick <nick@Nicks-MacBook-Pro.local>
2014-05-31 15:55:57 +1000

    add watchAcceleration

--- accelerometer.js
+++ accelerometer.js
@@ -30,6 +30,28 @@ angular.module('btford.phonegap.accelerometer',
             });
           }
         });
-      })
+      }),
+      watchAcceleration: phonegapReady(function (onSuccess, onError, options) {
+        navigator.accelerometer.watchAcceleration(
+          function () {
+
+            var that = this, args = arguments;
+            if (onSuccess) {
+              $rootScope.$apply(function () {
+                onSuccess.apply(that, args);
+              });
+            }
+          }, function () {
+            var that = this,
+              args = arguments;
+            if (onError) {
+              $rootScope.$apply(function () {
+                onError.apply(that, args);
+              });
+            }
+          },
+          options
+        )
+      }) // watchAcceleration
     };
   });
